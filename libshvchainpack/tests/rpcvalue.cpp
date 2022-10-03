2d60ffabc22b6732f37b694e0d17b70e306d697d
Václav Kubernát <sir.venceslas@gmail.com>
2022-10-03 19:28:19 +0200

    libshvchainpack: Add testing with exceptions

--- libshvchainpack/tests/rpcvalue.cpp
+++ libshvchainpack/tests/rpcvalue.cpp
@@ -1,4 +1,5 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <shv/chainpack/cponreader.h>
 #include <shv/chainpack/rpcvalue.h>
 #include <doctest/doctest.h>
 #include <optional>
@@ -7,43 +8,52 @@
 DOCTEST_TEST_CASE("RpcValue")
 {
 	std::string input;
-	std::optional<std::string> expected_output;
 
-	DOCTEST_SUBCASE("simple inputs")
+	DOCTEST_SUBCASE("fromCpon-toCpon idempotency")
 	{
-		DOCTEST_SUBCASE("numeric value")
+		std::optional<std::string> expected_output;
+		DOCTEST_SUBCASE("simple inputs")
 		{
-			input = "1";
-		}
-
-		DOCTEST_SUBCASE("string value")
-		{
-			input = R"("ahoj")";
-		}
-
-		DOCTEST_SUBCASE("datetime value")
-		{
-			DOCTEST_SUBCASE("simple case") // TODO give me a better name
+			DOCTEST_SUBCASE("numeric value")
 			{
-				input = R"(d"2018-02-02 0:00:00.001")";
-				expected_output = R"(d"2018-02-02T00:00:00.001Z")";
+				input = "1";
 			}
 
-			DOCTEST_SUBCASE("weird timezone")
+			DOCTEST_SUBCASE("string value")
 			{
-				input = R"(d"2041-03-04 0:00:00-1015")";
-				expected_output = R"(d"2041-03-04T00:00:00-1015")";
+				input = R"("ahoj")";
+			}
+
+			DOCTEST_SUBCASE("datetime value")
+			{
+				DOCTEST_SUBCASE("simple case") // TODO give me a better name
+				{
+					input = R"(d"2018-02-02 0:00:00.001")";
+					expected_output = R"(d"2018-02-02T00:00:00.001Z")";
+				}
+
+				DOCTEST_SUBCASE("weird timezone")
+				{
+					input = R"(d"2041-03-04 0:00:00-1015")";
+					expected_output = R"(d"2041-03-04T00:00:00-1015")";
+				}
 			}
 		}
+
+		if (!expected_output.has_value()) {
+			expected_output = input;
+		}
+
+		std::string err;
+		auto rpc_value = shv::chainpack::RpcValue::fromCpon(input, &err);
+		auto output = rpc_value.toCpon();
+		REQUIRE(output == *expected_output);
+		REQUIRE(err.empty());
 	}
 
-	if (!expected_output.has_value()) {
-		expected_output = input;
+	DOCTEST_SUBCASE("fromCpon invalid inputs")
+	{
+		input = "invalid input";
+		REQUIRE_THROWS_AS(shv::chainpack::RpcValue::fromCpon(input), shv::chainpack::ParseException);
 	}
-
-	std::string err;
-	auto rpc_value = shv::chainpack::RpcValue::fromCpon(input, &err);
-	auto output = rpc_value.toCpon();
-	REQUIRE(output == *expected_output);
-	REQUIRE(err.empty());
 }
