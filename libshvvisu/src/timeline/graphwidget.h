b7c8d50941bc6e6c3351e2df573523a7c16ef946
Martin Špirk <mspirk@elektroline.cz>
2023-10-04 10:52:18 +0200

    show open hand cursor above graph header

--- libshvvisu/src/timeline/graphwidget.h
+++ libshvvisu/src/timeline/graphwidget.h
@@ -62,6 +62,7 @@ protected:
 	void createProbe(qsizetype channel_ix, timemsec_t time);
 	void removeProbes(qsizetype channel_ix);
 	bool isMouseAboveChannelResizeHandle(const QPoint &mouse_pos) const;
+	bool isMouseAboveChannelHeader(const QPoint &mouse_pos) const;
 	bool isMouseAboveMiniMap(const QPoint &mouse_pos) const;
 	bool isMouseAboveMiniMapHandle(const QPoint &mouse_pos, bool left) const;
 	bool isMouseAboveLeftMiniMapHandle(const QPoint &pos) const;
