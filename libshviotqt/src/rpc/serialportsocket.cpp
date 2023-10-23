2b9da7c6f54b381470d1a0ca7bdf01596ca3731d
Václav Kubernát <vkubernat@elektroline.cz>
2023-10-23 13:58:30 +0200

    Remove deprecated qAsConst
    Deprecated from 6.6.

--- libshviotqt/src/rpc/serialportsocket.cpp
+++ libshviotqt/src/rpc/serialportsocket.cpp
@@ -232,7 +232,7 @@ void SerialPortSocket::onSerialDataReadyRead()
 				}
 				if(!m_readMessageCrcBuffer.inEscape && m_readMessageCrcBuffer.data.size() == sizeof(shv::chainpack::crc32_t)) {
 					shv::chainpack::crc32_t msg_crc = 0;
-					for(uint8_t bb : qAsConst(m_readMessageCrcBuffer.data)) {
+					for(uint8_t bb : std::as_const(m_readMessageCrcBuffer.data)) {
 						msg_crc <<= 8;
 						msg_crc += bb;
 					}
