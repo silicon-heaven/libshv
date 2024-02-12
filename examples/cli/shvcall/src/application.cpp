00bb1dae514bf8edb776996bdcfc9712cbccedad
Václav Kubernát <vkubernat@elektroline.cz>
2024-02-12 17:37:47 +0100

    shvcall: Add subscription mode

--- examples/cli/shvcall/src/application.cpp
+++ examples/cli/shvcall/src/application.cpp
@@ -5,6 +5,7 @@
 #include <shv/coreqt/log.h>
 
 #include <iostream>
+#include <regex>
 
 namespace cp = shv::chainpack;
 namespace si = shv::iotqt;
@@ -23,10 +24,70 @@ Application::Application(int &argc, char **argv, AppCliOptions *cli_opts)
 	m_rpcConnection->open();
 }
 
+namespace placeholders {
+#define PLACEHOLDER_REGEX(placeholder_str) const auto placeholder_str = std::regex("\\{" #placeholder_str "\\}")
+PLACEHOLDER_REGEX(TIME);
+PLACEHOLDER_REGEX(PATH);
+PLACEHOLDER_REGEX(METHOD);
+PLACEHOLDER_REGEX(VALUE);
+}
+
 void Application::onShvStateChanged()
 {
 	if (m_rpcConnection->state() == si::rpc::ClientConnection::State::BrokerConnected) {
 		cp::RpcValue params;
+		if (m_cliOptions->shouldSubscribe()) {
+			auto sub_id = m_rpcConnection->callMethodSubscribe(m_cliOptions->path(), m_cliOptions->method());
+			connect(m_rpcConnection, &shv::iotqt::rpc::ClientConnection::rpcMessageReceived, this, [this, sub_id] (const shv::chainpack::RpcMessage& msg)  {
+				if (msg.isResponse()) {
+					auto log_error = [this] (const auto& error_msg) {
+						shvError() << "Couldn't subscribe to:" << m_cliOptions->path() << m_cliOptions->method() << "msg:" << error_msg;
+						exit(1);
+					};
+					auto response = shv::chainpack::RpcResponse(msg);
+					if (response.requestId() != sub_id) {
+						return;
+					}
+
+					if (response.isError()) {
+						log_error(response.error().toString());
+						return;
+					}
+
+					if (response.result().toBool() != true) {
+						log_error("Unexpected error.");
+						return;
+					}
+
+					shvDebug() << "Subscription successful.";
+					return;
+				}
+
+				if (msg.isSignal()) {
+					const auto signal = shv::chainpack::RpcSignal(msg);
+					auto res = m_cliOptions->subscribeFormat();
+					const auto do_replace = [&res] (const std::regex& regex, const auto& replacement_fn) { // clazy:exclude=lambda-in-connect - https://bugs.kde.org/show_bug.cgi?id=443342
+						std::smatch result;
+						if (!std::regex_search(res, result, regex)) {
+							return;
+						}
+
+						const auto replacement = replacement_fn();
+						for (const auto& match : result) {
+							res.replace(match.first, match.second, replacement);
+						}
+					};
+					do_replace(placeholders::TIME, [] { return shv::chainpack::RpcValue::DateTime::now().toIsoString(); });
+					do_replace(placeholders::PATH, [&signal] { return signal.shvPath().asString(); });
+					do_replace(placeholders::METHOD, [&signal] { return signal.method().asString(); });
+					do_replace(placeholders::VALUE, [&signal] { return signal.params().toCpon(); });
+					std::cout << res << '\n';
+					return;
+				}
+			});
+			return;
+		}
+
 		try {
 			params = m_cliOptions->params().empty() ? cp::RpcValue() : cp::RpcValue::fromCpon(m_cliOptions->params());
 		} catch (std::exception&) {
