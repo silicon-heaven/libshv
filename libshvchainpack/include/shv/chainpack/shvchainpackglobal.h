9a88fcb040d549728d8e0f4f2090b81eaba0fd3f
Václav Kubernát <vkubernat@elektroline.cz>
2024-05-02 17:54:17 +0200

    Fix usage of reserved identifiers

--- libshvchainpack/include/shv/chainpack/shvchainpackglobal.h
+++ libshvchainpack/include/shv/chainpack/shvchainpackglobal.h
@@ -1,19 +1,19 @@
 #pragma once
 
 #if defined _WIN32
-#define _SHVCHAINPACK_DECL_EXPORT     __declspec(dllexport)
-#define _SHVCHAINPACK_DECL_IMPORT     __declspec(dllimport)
+#define SHVCHAINPACK_DECL_EXPORT_IMPL     __declspec(dllexport)
+#define SHVCHAINPACK_DECL_IMPORT_IMPL     __declspec(dllimport)
 #else
-#define _SHVCHAINPACK_DECL_EXPORT     __attribute__((visibility("default")))
-#define _SHVCHAINPACK_DECL_IMPORT     __attribute__((visibility("default")))
-#define _SHVCHAINPACK_DECL_HIDDEN     __attribute__((visibility("hidden")))
+#define SHVCHAINPACK_DECL_EXPORT_IMPL     __attribute__((visibility("default")))
+#define SHVCHAINPACK_DECL_IMPORT_IMPL     __attribute__((visibility("default")))
+#define SHVCHAINPACK_DECL_HIDDEN_IMPL     __attribute__((visibility("hidden")))
 #endif
 
 /// Declaration of macros required for exporting symbols
 /// into shared libraries
 #if defined(SHVCHAINPACK_BUILD_DLL)
-#define SHVCHAINPACK_DECL_EXPORT _SHVCHAINPACK_DECL_EXPORT
+#define SHVCHAINPACK_DECL_EXPORT SHVCHAINPACK_DECL_EXPORT_IMPL
 #else
-#define SHVCHAINPACK_DECL_EXPORT _SHVCHAINPACK_DECL_IMPORT
+#define SHVCHAINPACK_DECL_EXPORT SHVCHAINPACK_DECL_IMPORT_IMPL
 #endif
 
