e1c86ab07fc02164ab1e752747202d8c1e53dae3
Václav Kubernát <vkubernat@elektroline.cz>
2024-05-02 17:51:12 +0200

    Fix empty catch warning

--- libshvchainpack/fuzz/fromChainpack.cpp
+++ libshvchainpack/fuzz/fromChainpack.cpp
@@ -5,6 +5,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
     try {
         auto rpc_value = shv::chainpack::RpcValue::fromChainPack({reinterpret_cast<const char*>(data), size}, &err);
     } catch(std::exception& ex) {
+        return 0;
     }
     return 0;  // Values other than 0 and -1 are reserved for future use.
 }
