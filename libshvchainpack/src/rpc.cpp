be55f5abaef00a9854b433dc76cd4747944c9815
Václav Kubernát <vkubernat@elektroline.cz>
2025-03-04 15:20:39 +0100

    Fix linking issues on windows
    These should be redundant, but they are needed for Debug builds on MinGW.
    D:/a/shvspy/shvspy/3rdparty/libshv/libshviotqt/src/rpc/clientconnection.cpp:357: undefined reference to `__imp__ZN3shv9chainpack3Rpc13RCV_LOG_ARROWE'

--- libshvchainpack/src/rpc.cpp
+++ libshvchainpack/src/rpc.cpp
@@ -1,6 +1,10 @@
 #include <shv/chainpack/rpc.h>
 
 namespace shv::chainpack {
+// NOLINTBEGIN(readability-redundant-declaration) - needed to fix debug build on MinGW
+constexpr const char* Rpc::RCV_LOG_ARROW;
+constexpr const char* Rpc::PAR_SIGNAL;
+// NOLINTEND(readability-redundant-declaration)
 
 const char *Rpc::protocolTypeToString(Rpc::ProtocolType pv)
 {
