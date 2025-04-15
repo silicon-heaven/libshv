554ec16047c35351981296b16d0c25daf5d9a6b4
Fanda Vacek <fvacek@elektroline.cz>
2025-04-15 15:48:42 +0200

    Generate CRC32 lookup table by compiler

--- libshvchainpack/tests/test_crc32.cpp
+++ libshvchainpack/tests/test_crc32.cpp
@@ -18,7 +18,7 @@ DOCTEST_TEST_CASE("CRC SHV3")
 	shv::chainpack::Crc32Shv3 crc;
 	vector<uint8_t> data = { 0x01,0x8B,0x41,0x41,0x48,0x41,0x49,0x86,0x07,0x66,0x6F,0x6F,0x2F,0x62,0x61,0x72,0x4A,0x86,0x03,0x62,0x61,0x7A,0xFF,0x8A,0x41,0x86,0x05,0x68,0x65,0x6C,0x6C,0x6F,0xFF };
 	crc.add(data.data(), data.size());
-	auto my_crc = crc.finalize();
+	auto my_crc = crc.result();
 
 	REQUIRE(0xd7a14e96 == my_crc);
 }
@@ -36,10 +36,10 @@ DOCTEST_TEST_CASE("CRC on strings")
 		auto data = reinterpret_cast<const unsigned char*>(s.data());
 		zip_crc = crc32(zip_crc, data, static_cast<unsigned int>(s.size()));
 
-		shv::chainpack::Crc32 crc;
+		shv::chainpack::Crc32Posix crc;
 		crc.add(s.data(), s.size());
 		CAPTURE(s);
-		REQUIRE(zip_crc == crc.finalize());
+		REQUIRE(zip_crc == crc.result());
 	}
 }
 
