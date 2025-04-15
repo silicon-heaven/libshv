b80e7f83742243b618662dc6a7c380b1724792f8
Fanda Vacek <fvacek@elektroline.cz>
2025-04-15 15:42:45 +0200

    Fix CRC32 impl

--- libshvchainpack/tests/test_crc32.cpp
+++ libshvchainpack/tests/test_crc32.cpp
@@ -13,30 +13,12 @@ using namespace shv::chainpack;
 using namespace std;
 using namespace std::string_literals;
 
-DOCTEST_TEST_CASE("CRC on CRC_POSIX_POLY")
-{
-	constexpr auto N = sizeof(CRC_POSIX_POLY_REV);
-	array<uint8_t, N> arr;
-	for(size_t i = 0 ; i < N; ++i) {
-		arr[i] = static_cast<uint8_t>(CRC_POSIX_POLY_REV >> (i * 8));
-	}
-	auto zip_crc = ::crc32(0L, Z_NULL, 0);
-	auto data = reinterpret_cast<const unsigned char*>(arr.data());
-	zip_crc = ::crc32(zip_crc, data, arr.size());
-
-	shv::chainpack::Crc32Posix crc;
-	crc.add(arr.data(), arr.size());
-	auto my_crc = crc.remainder();
-
-	REQUIRE(zip_crc == my_crc);
-}
-
 DOCTEST_TEST_CASE("CRC SHV3")
 {
 	shv::chainpack::Crc32Shv3 crc;
 	vector<uint8_t> data = { 0x01,0x8B,0x41,0x41,0x48,0x41,0x49,0x86,0x07,0x66,0x6F,0x6F,0x2F,0x62,0x61,0x72,0x4A,0x86,0x03,0x62,0x61,0x7A,0xFF,0x8A,0x41,0x86,0x05,0x68,0x65,0x6C,0x6C,0x6F,0xFF };
 	crc.add(data.data(), data.size());
-	auto my_crc = crc.remainder();
+	auto my_crc = crc.finalize();
 
 	REQUIRE(0xd7a14e96 == my_crc);
 }
@@ -54,10 +36,10 @@ DOCTEST_TEST_CASE("CRC on strings")
 		auto data = reinterpret_cast<const unsigned char*>(s.data());
 		zip_crc = crc32(zip_crc, data, static_cast<unsigned int>(s.size()));
 
-		shv::chainpack::Crc32Posix crc;
+		shv::chainpack::Crc32 crc;
 		crc.add(s.data(), s.size());
 		CAPTURE(s);
-		REQUIRE(zip_crc == crc.remainder());
+		REQUIRE(zip_crc == crc.finalize());
 	}
 }
 
