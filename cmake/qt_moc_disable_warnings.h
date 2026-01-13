ae29af9dd3412a5e0d55c2f95fdc25c622f96d65
Václav Kubernát <vkubernat@elektroline.cz>
2026-01-13 15:36:38 +0100

    qt_moc: Ignore warning

--- cmake/qt_moc_disable_warnings.h
+++ cmake/qt_moc_disable_warnings.h
@@ -1,4 +1,9 @@
 #ifdef __GNUC__
+#ifndef __clang__
 #pragma GCC diagnostic ignored "-Wuseless-cast"
 #endif
+#endif
 
+#ifdef __clang__
+#pragma GCC diagnostic ignored "-Wreserved-identifier"
+#endif
